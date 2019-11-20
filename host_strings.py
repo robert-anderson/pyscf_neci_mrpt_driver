99f628ed943fc4b4f861d111660409a531db9dbd
Robert Anderson <k1507071@login3.pri.rosalind2.alces.network>
2019-11-20 12:11:37 +0000

    updated for changes to morty environment

--- host_strings.py
+++ host_strings.py
@@ -43,11 +43,12 @@ class SubmitScript(FileBlock):
         self << header
         self << 'WD={wd}'
         self << 'cd $WD'
-        if scheduler=='slurm':
+        #if scheduler=='slurm':
+        if 0:
             self << 'if [ {nnodes} == 1 ]; then'
             self << '  tmpdir=$(mktemp -d /tmp$HOME/tmp.XXXXXXXXXX)'
             self << '  echo Working on a single node "${{SLURM_NODELIST}}", so using a temporary directory "${{tmpdir}}" on-node'
-            self << '  cp FCIDUMP *.inp nevpt2_store.pkl $tmpdir'
+            self << '  cp {files_to_node} $tmpdir'
             self << '  cd $tmpdir'
             self << 'fi'
 
@@ -80,16 +81,17 @@ class SubmitScript(FileBlock):
             '. /users/k1507071/.bashrc' <<\
             'module purge' <<\
             'export OMP_NUM_THREADS=32' <<\
-            'module load {0}/gcc/7.4.0 {0}/openmpi/3.1.4/gnu_7.4.0 {0}/openblas/0.3.6 {0}/lapack/3.8.0'.format(
-                    '/users/k1507071/opt/modules')<<\
+            'module load libs/lapack/3.8.0-gcc7.3.0 libs/openmpi/4.0.0-gcc7.3.0 libs/openblas/0.3.6-target-skylakex-gcc7.3.0' <<\
             'conda activate 2.7'
+
+            #'module load {0}/gcc/7.4.0 {0}/openmpi/ib/3.1.4/gnu_7.4.0 {0}/openblas/0.3.6 {0}/lapack/3.8.0'.format(
+            #        '/users/k1507071/opt/modules')<<\
 
         env_setup <<\
         'export PYTHONPATH={pyscf_dir}:$PYTHONPATH' <<\
         'export PYTHONPATH={driver_dir}:$PYTHONPATH'
         self << env_setup
     
-        
         processing = FileBlock()
         processing <<\
         'function copy_back () {{' <<\
@@ -177,9 +179,10 @@ class SubmitScript(FileBlock):
         'fi'
         self << processing
     def render(self, fname, **kwargs):
-        if 'partition' not in kwargs: kwargs['partition'] = '"morty,nodes"'
+        if 'partition' not in kwargs: kwargs['partition'] = '"morty,nms_research"'
         kwargs['driver_dir'] = os.path.abspath(__file__)
         kwargs['ncores'] = kwargs['nnodes']*kwargs['ntaskspernode']
+        kwargs['files_to_node'] = ' '.join(kwargs['files_to_node'])
         FileBlock.render(self, fname, **kwargs)
 
 class WfInitScript(SubmitScript):
@@ -207,18 +210,175 @@ class Nevpt2Script(SubmitScript):
         '  i=$(ls neci.out* | wc -l)' <<\
         '  if [ $i = 0 ]; then' <<\
         '    echo "initialising FCIQMC calculation"' <<\
-        '    mpirun -np {ncores} {neci_exe} initial_hbrdm.inp > tmp.out' <<\
+        '    mpirun -np {ncores} {neci_exe} initial_hbrdm.inp > tmp.out 2> tmp.err' <<\
         '  elif [ {reset_period} != 0 ] && [ $(echo "$i%{reset_period}" | bc) = 0 ]; then' <<\
         '    echo "resetting RDM accumulations and continuing from scratch"' <<\
-        '    mpirun -np {ncores} {neci_exe} reset_hbrdm.inp > tmp.out' <<\
+        '    mpirun -np {ncores} {neci_exe} reset_hbrdm.inp > tmp.out 2> tmp.err' <<\
         '  else'
         if step_seed: execution << '    sed -i s/"seed.*"/"seed $i"/g continue_hbrdm.inp'
         execution <<\
         '    echo "continuing an existing RDM accumulation"' <<\
-        '    mpirun -np {ncores} {neci_exe} continue_hbrdm.inp > tmp.out' <<\
+        '    mpirun -np {ncores} {neci_exe} continue_hbrdm.inp > tmp.out 2> tmp.err' <<\
         '  fi' <<\
         '  postprocess'
         if not loop: execution << '  clean_up_and_quit'
         execution << '  save_pops'
         execution << 'done'
         self << execution
+    def render(self, fname, **kwargs):
+        kwargs['files_to_node'] = ('FCIDUMP', '*.inp', 'nevpt2_store.pkl')
+        SubmitScript.render(self, fname, **kwargs)
+
+class Caspt2Script(FileBlock):
+    def __init__(self):
+        FileBlock.__init__(self)
+        domain = get_domain()
+        scheduler = schedulers[domain]
+        token = tokens[scheduler]
+
+        header = FileBlock()
+
+        if scheduler=='sge':
+            header <<\
+            ' -S /bin/bash' <<\
+            ' -N {name}' <<\
+            ' -l h_rt={time}' <<\
+            ' -l h_vmem=2G' <<\
+            ' -M robert.anderson@kcl.ac.uk' <<\
+            ' -cwd' <<\
+            ' -pe mpi-16 {ncores}' <<\
+            ' -m s'
+        elif scheduler=='slurm':
+            self << '#!/bin/bash -l'
+            header <<\
+            ' --nodes={nnodes}' <<\
+            ' --partition={partition}' <<\
+            ' --ntasks-per-node={ntaskspernode}' <<\
+            ' --time={time}' <<\
+            ' --job-name={name}'
+
+        header.prefix_all(token)
+
+        self << header
+        self << 'WD={wd}'
+        self << 'cd $WD'
+        #if scheduler=='slurm':
+        if 0:
+            self << 'if [ {nnodes} == 1 ]; then'
+            self << '  tmpdir=$(mktemp -d /tmp$HOME/tmp.XXXXXXXXXX)'
+            self << '  echo Working on a single node "${{SLURM_NODELIST}}", so using a temporary directory "${{tmpdir}}" on-node'
+            self << '  cp {files_to_node} $tmpdir'
+            self << '  cd $tmpdir'
+            self << 'fi'
+
+        env_setup = FileBlock()
+
+        if domain=='prv.rosalind.compute.estate':
+            env_setup <<\
+            'module purge' <<\
+            'module load general/python/2.7.10' <<\
+            'module load libs/openmpi/2.0.0/gcc5.3.0' <<\
+            'export LD_LIBRARY_PATH=/opt/apps/libs/openblas/gcc/0.2.19/lib:$LD_LIBRARY_PATH' <<\
+            'export LIBRARY_PATH=/opt/apps/libs/openblas/gcc/0.2.19/lib:$LIBRARY_PATH' <<\
+            'export INCLUDE=/opt/apps/libs/openblas/gcc/0.2.19/include:$INCLUDE' <<\
+            'export C_INCLUDE_PATH=/opt/apps/libs/openblas/gcc/0.2.19/include:$C_INCLUDE_PATH' <<\
+            'MKLROOT=/opt/apps/intel/composer_xe_2015.3.187/mkl' <<\
+            'export LD_LIBRARY_PATH=/opt/apps/intel/composer_xe_2015.3.187/mkl/lib/intel64:$LD_LIBRARY_PATH' <<\
+            'export LD_LIBRARY_PATH=/opt/apps/intel/composer_xe_2015.3.187/compiler/lib/intel64:$LD_LIBRARY_PATH' <<\
+            'export LD_PRELOAD=$MKLROOT/lib/intel64/libmkl_core.so:$MKLROOT/lib/intel64/libmkl_sequential.so' <<\
+            '. /users/k1507071/virtualenvs/python2/bin/activate' <<\
+            'export OMP_NUM_THREADS=1'
+        elif domain=='thomas.ucl.ac.uk':
+            env_setup <<\
+            'module load general/python/2.7.10' <<\
+            'module load libs/openblas/0.2.19/gcc5.3.0' <<\
+            'module unload libs/openmpi/1.10.2/gcc5.3.0' <<\
+            'module load libs/openmpi/2.0.1/intel15.0' <<\
+            'module unload python/3.6.3 python3/3.6 python3/recommended; module load python2/recommended'
+        elif domain=='pri.rosalind2.alces.network':
+            env_setup <<\
+            '. /users/k1507071/.bashrc' <<\
+            'module purge' <<\
+            'export OMP_NUM_THREADS=32' <<\
+            'module load {0}/gcc/7.4.0 {0}/boost/1.70/gcc7.4.0/python2.7'.format('/users/k1507071/opt/modules') <<\
+            'module load libs/lapack/3.8.0-gcc7.3.0 libs/openmpi/4.0.0-gcc7.3.0 libs/openblas/0.3.6-target-skylakex-gcc7.3.0' <<\
+            '. $HOME/intel/mkl/bin/mklvars.sh intel64'
+            #'module load {} libs/lapack/3.8.0/gcc-7.3.0'.format('/opt/apps/etc/modules/libs/openmpi/4.0.0/gcc/7.3.0') <<\
+        self << env_setup
+        
+        processing = FileBlock()
+        processing <<\
+        'function copy_back () {{' <<\
+        '  if [ -e $tmpdir ]; then' <<\
+        '    rsync $tmpdir/*POPSFILE* $WD' <<\
+        '    rsync $tmpdir/spinfree* $WD' <<\
+        '    rsync $tmpdir/FCIMCStats* $WD' <<\
+        '    rsync $tmpdir/CORESPACE $WD' <<\
+        '    rsync $tmpdir/CICJ_HISTOGRAM $WD' <<\
+        '    rsync $tmpdir/caspt2.energy.dat $WD' <<\
+        '    rsync $tmpdir/neci.out.* $WD' <<\
+        '    rsync $tmpdir/RDMEstimates $WD' <<\
+        '  fi' <<\
+        '}}' <<\
+        'function clean_up_and_quit () {{' <<\
+        '  copy_back' <<\
+        '  exit' <<\
+        '}}' <<\
+        'function trailing_float () {{' <<\
+        '  echo $1 | grep -Eo "\-?[0-9]+\.[0-9]+\s*$"' <<\
+        '}}' <<\
+        'function get_output_value () {{' <<\
+        '  trailing_float "$(grep $2 $1)"' <<\
+        '}}' <<\
+        'function postprocess {{' <<\
+        '  if [ $(tail -n3 tmp.out | grep ended -c) == 0 ]; then' <<\
+        '    echo "NECI failure detected. Exiting"' <<\
+        '    exit' <<\
+        '  fi' <<\
+        '  i=$(ls neci.out* | wc -l)' <<\
+        '  E_2rdm=$(grep "TOTAL ENERGY" tmp.out | grep -Eo "\-?[0-9]+.*")' <<\
+        '  Herm_3rdm=$(grep "HBRDM HERMITICITY" tmp.out | grep -Eo "\-?[0-9]+.*")'
+
+        processing <<\
+        '  mv tmp.out neci.out.$i' <<\
+        '  mv FCIMCStats FCIMCStats.$i; mv FCIMCStats2 FCIMCStats2.$i' <<\
+        '  cp *POPSFILE* last_pops' <<\
+        '}}'
+
+        processing <<\
+        'if [ ! -e last_pops ]; then' <<\
+        '  mkdir last_pops' <<\
+        'fi'
+        self << processing
+        
+        execution = FileBlock()
+        execution <<\
+        'echo "initialising FCIQMC calculation"' <<\
+        'mpirun -np {ncores} {neci_exe} initial_hbrdm.inp > neci.out.0' <<\
+        'rm fciqmc_0_0.rdm*' <<\
+        'ln -s spinfree_OneRDM.1 fciqmc_0_0.rdm1' <<\
+        'ln -s spinfree_TwoRDM.1 fciqmc_0_0.rdm2' <<\
+        'ln -s spinfree_ThreeRDM.1 fciqmc_0_0.rdm3' <<\
+        'ln -s spinfree_CASPT2_AUX.1 fciqmc_0_0.rdm4f' <<\
+        'smith_line=$(grep smith bagel.json)' <<\
+        'export OMP_NUM_THREADS={ncores}'
+        for exp in (5, 6):
+            execution << r'sed s/"$smith_line"/"$smith_line \"thresh_overlap\": 1e-{},"/g bagel.json > tmp.json'.format(exp)
+            execution << '{bagel_exe} tmp.json > bagel.out.0.'+str(exp)
+        execution <<\
+        'while true; do' <<\
+        'iter=$(ls neci.out.* | wc -l)' <<\
+        'mpirun -np {ncores} {neci_exe} continue_hbrdm.inp > neci.out.$iter' <<\
+        'smith_line=$(grep smith bagel.json)'
+        for exp in (3, 4, 5, 6):
+            execution << r'sed s/"$smith_line"/"$smith_line \"thresh_overlap\": 1e-{},"/g bagel.json > tmp.json'.format(exp)
+            execution << '{bagel_exe} tmp.json > bagel.out.$iter.'+str(exp)
+        execution << 'done'
+        self << execution
+    def render(self, fname, **kwargs):
+        if 'partition' not in kwargs: kwargs['partition'] = '"morty,nodes"'
+        kwargs['driver_dir'] = os.path.abspath(__file__)
+        kwargs['ncores'] = kwargs['nnodes']*kwargs['ntaskspernode']
+        kwargs['files_to_node'] = 'FCIDUMP *.inp FOCKMAT ref.archive bagel.json'
+        FileBlock.render(self, fname, **kwargs)
+
--- neci_strings.py
+++ neci_strings.py
@@ -1,10 +1,16 @@
 def write_inp(fname, electrons, nwalkers, nmcyc=-1, time=None, write_wf=False, read_wf=False, write_rdm=False, shiftdamp=0.05, memoryfacspawn=20.0,
-        read_rdm=False, granularity=1, cicj_promotion_hwhm_fraction=0, pops_core=0, write_core=False, read_core=False, two_rdm_only=False, stepsshift=5,
+        read_rdm=False, granularity=1, promote_fractions=1.0, pops_core=0, write_core=False, read_core=False, two_rdm_only=False, stepsshift=5, max_rank_hbrdm_semi_stoch_fill=4,
         ss_start=0, rdm_start=0, rdm_iters=0, rdm_energy_iters=None, walkcontgrow=False, seed=14, main_facs=(1,)*5, spawn_facs=(1,)*5, recv_facs=(1,)*5,
-        hbrdm_offdiag_frac_occ_thresh=0.0, tau='', trial_start=0, pops_trial=0, niter_cicj_histogram=100, cicj_promotion_scale=1.0):
+        hbrdm_offdiag_frac_occ_thresh=0.0, tau='', trial_start=0, pops_trial=0, mrpt=None):
     if main_facs is not None: main_facs = ' '.join(map(str, main_facs))
     if spawn_facs is not None: spawn_facs = ' '.join(map(str, spawn_facs))
     if recv_facs is not None: recv_facs = ' '.join(map(str, recv_facs))
+    if promote_fractions is not None:
+        try:
+            promote_fractions+0
+            promote_fractions = str(promote_fractions)
+        except ValueError:
+            promote_fractions = ' '.join(map(str, promote_fractions))
     with open(fname, 'w') as f:
         f.write('''title
 system read noorder
@@ -54,7 +60,7 @@ nmcyc {}
             f.write('pops-core {}\n'.format(pops_core))
         if pops_trial > 0:
             if trial_start:
-                f.write('trial-wavefunction {}\n'.format(trial_start))
+                f.write('trial-wavefunction {}\n'.format(min(trial_start, rdm_start) if write_rdm else trial_start))
             else:
                 f.write('trial-wavefunction\n')
             f.write('pops-trial {}\n'.format(pops_trial))
@@ -71,12 +77,12 @@ logging
         if write_rdm:
             if rdm_energy_iters is None: rdm_energy_iters = rdm_iters
             f.write('calcrdmonfly 3 {} {}\n'.format(rdm_start, rdm_energy_iters))
-            if not two_rdm_only:
-                f.write('nevpt2\n')
+            if not two_rdm_only and mrpt is not None:
+                assert(mrpt.lower() in ('nevpt2', 'caspt2'))
+                f.write('{}\n'.format(mrpt))
                 f.write('hbrdm-ghost-granularity {}\n'.format(granularity))
-                f.write('cicj-promotion-hwhm-fraction {}\n'.format(cicj_promotion_hwhm_fraction))
-                f.write('cicj-promotion-scale {}\n'.format(cicj_promotion_scale))
-                f.write('niter-cicj-histogram {}\n'.format(niter_cicj_histogram))
+                f.write('4rdm-promotion-fractions {}\n'.format(promote_fractions))
+                f.write('max-rank-hbrdm-semi-stoch-fill {}\n'.format(max_rank_hbrdm_semi_stoch_fill))
             f.write('write-spin-free-rdm\n')
             f.write('printonerdm\n')
             if main_facs is not None: f.write('rdm-main-size-fac {}\n'.format(main_facs))
